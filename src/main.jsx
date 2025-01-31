d37fbb5ac87d15b3643d9556992b39cf6e8545b6
prakharDatacode <prakhar.datacode@gmail.com>
2025-01-31 11:25:51 +0530

    Completed-HomePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import "bootstrap/dist/css/bootstrap.css"; // Bootstrap JS for components
-import "bootstrap/dist/js/bootstrap.bundle.min"; // Required for toggle functionality
+import "bootstrap/dist/css/bootstrap.css";
 import App from './App.jsx'
 import "./assets/styles/formAssembly.scss"
 createRoot(document.getElementById('root')).render(
